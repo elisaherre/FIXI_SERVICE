14c0a1cc5b80cd1b0419f94c123c5b722910a4fe
Facundo Quiroga <facu.quirogaga@gmail.com>
2023-09-06 15:52:19 -0500

    Arreglada ruta del show del fixer y creada la ruta del perfil

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'profile/id', to: "profiles#show", as: "profile"
+  get 'fixers/:id', to: "fixers#show", as: "fixer"
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :fixers
   # Defines the root path route ("/")
   # root "articles#index"
   get "categories/:id", to: "categories#show", as: "category"
