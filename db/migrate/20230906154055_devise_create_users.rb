03e9d88a79cb33180d82e749b8199b0cc6cc9e3e
Elisa Herrera <elisaherrera903@gmail.com>
2023-09-06 09:58:20 -0600

    creamos modelo category y editamos columnas de user

--- db/migrate/20230906154055_devise_create_users.rb
+++ db/migrate/20230906154055_devise_create_users.rb
@@ -6,6 +6,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :address
+      t.string :name
+      t.string :phone
+      t.float :latitude
+      t.float :longitude
 
       ## Recoverable
       t.string   :reset_password_token
