c8464462ca9dc8b5efbf518c1a2f095c1feae91f
dominique cancino <cs.dominique17@gmail.com>
2023-09-07 18:29:50 -0300

    se arreglo dropdown

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   def show
+    @categories = Category.all
     @category = Category.find(params[:id])
     @fixers = User.all.select do |user|
       user.categories.include?(@category)
