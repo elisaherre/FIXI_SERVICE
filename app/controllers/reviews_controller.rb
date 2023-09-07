cf2f4c62155477381f5103530d3dd82e69a0ff4d
Elisa Herrera <elisaherrera903@gmail.com>
2023-09-06 21:19:34 -0600

    modificamos los reviews para diferenciar a los fixers

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,10 +6,20 @@ class ReviewsController < ApplicationController
 
   def create
     # user_id, request_id, content, rating
-    @review = Review.new(review_params)
     @request = Request.find(params[:request_id])
+    @service = Service.find(@request.service_id)
+    @fixer = User.find(@service.user_id)
+
+    @review = Review.new(review_params)
+
+    if @review.user_id
+      @review.user_id = params[:review][:user_id] # el fixer califica al usuario
+    else
+      @review.user_id = @fixer.id # el usuario califica al fixer
+    end
+
     @review.request = @request
-    @review.user_id = @request.user_id
+
     if @review.save
       redirect_to requests_path
     else
@@ -20,6 +30,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:content, :rating)
+    params.require(:review).permit(:content, :rating, :user_id)
   end
 end
