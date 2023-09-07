48d6eb4c7c375cae82132fae8cd1abf55075a1c2
dominique cancino <cs.dominique17@gmail.com>
2023-09-06 21:34:43 -0300

    se creo controller service

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,41 @@
+class ServicesController < ApplicationController
+
+  def index
+   @services = Service.all
+  end
+
+  def show
+    @service = Service.find(params[:id])
+  end
+
+  #no lleva vista
+
+  def new
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new(service_params)
+
+    if @service.save
+      redirect_to service_path(@service), notice: 'Se ha creado un nuevo servicio.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @service = Service.find(params[:id])
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @service.destroy
+    redirect_to services_path, status: :see_other
+  end
+
+end
