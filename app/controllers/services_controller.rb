72da4febe65b0c762937114e78dd75b6e0315fcc
dominique cancino <cs.dominique17@gmail.com>
2023-09-07 11:55:38 -0300

    actualizacion services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -30,12 +30,22 @@ class ServicesController < ApplicationController
   end
 
   def update
-
+    @service = Service.find(params[:id])
+    @service.update(service_params)
+    redirect_to services_path(@services)
   end
 
   def destroy
+    @service = Service.find(params[:id])
     @service.destroy
-    redirect_to services_path, status: :see_other
+    # No need for app/views/restaurants/destroy.html.erb
+     redirect_to service_path, status: :see_other
   end
 
+end
+
+private
+
+def service_params
+  params.require(:services).permit(:user_id, :category_id, :description)
 end
