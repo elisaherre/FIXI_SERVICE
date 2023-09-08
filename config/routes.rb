9d44183d858c8ade62e96eda5769246cb4e74573
Elisa Herrera <elisaherrera903@gmail.com>
2023-09-08 13:57:35 -0600

    new requests funcionan a la mitad, falta pasar al fixer y sus categorías

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'profile', to: "profiles#show", as: :profile
   get 'fixers/:id', to: "fixers#show", as: :fixer
+
   patch 'requests/accept/:id', to: "requests#accept", as: :accept
   patch 'requests/cancel/:id', to: "requests#cancel", as: :cancel
   patch 'requests/book/:id', to: "requests#book", as: :book
@@ -9,15 +10,19 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: "registrations" }
   root to: "pages#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :services
 
   get "categories/:id", to: "categories#show", as: :category
-  resources :requests do
+
+  resources :services
+
+  resources :requests, only: [:index, :new, :create] do
     resources :reviews, only: [:new, :create]
   end
+
 
 end
