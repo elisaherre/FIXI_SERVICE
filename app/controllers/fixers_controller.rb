1ae606f3f1ee7566827bfbeb871c5500f33e5b4c
Facundo Quiroga <facu.quirogaga@gmail.com>
2023-09-12 15:49:09 -0500

    actualizada vista del show del fixer

--- app/controllers/fixers_controller.rb
+++ app/controllers/fixers_controller.rb
@@ -4,35 +4,19 @@ class FixersController < ApplicationController
     fixers_id = Service.all.map {|user| user.user_id }.uniq
     user = User.find_by(id: params[:id])
     @fixer = user if fixers_id.include? user.id
-    @services = Service.where(user_id: @fixer.id)
 
-    #iteraciones anidadas para llegar a las reviews del fixer
+    suma_rating = @fixer.reviews.map { |review| review.rating }.sum
+    @rating = suma_rating > 0 ? suma_rating / @fixer.reviews.count : 0
+
+    @trabajos = @fixer.requests.map { |request| request.status == "finalizada" }.count
+
+    @services = Service.where(user_id: @fixer.id)
     @reviews = []
     requests = @services.each do |service|
       service.requests.each do |request|
         request.reviews.each do |review|
           @reviews << review
         end
-      end
-    end
-
-    # Captura del promedio del rating
-    suma = 0
-    @reviews.each do |review|
-      suma += review.rating
-    end
-    if suma > 0
-      @average = suma / @reviews.size
-    else
-      @average = 0
-    end
-
-
-    # Captura de trabajos realizados
-    @completed = 0
-    @services.each do |service|
-      service.requests.each do |request|
-        @completed += 1 if request.status == "finalizado"
       end
     end
   end
